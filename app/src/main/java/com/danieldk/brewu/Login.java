f5540e2da62a9dfe5edf727a1f67913e4a4b1cac
Daniel Knudsen <dk@idealdev.dk>
2019-04-25 15:22:42 +0200

    Added email login Didnt clean up login from original

--- app/src/main/java/com/danieldk/brewu/Login.java
+++ app/src/main/java/com/danieldk/brewu/Login.java
@@ -8,17 +8,36 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.client.AuthUiInitProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Login extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 1;
 
     Button btn_login;
     EditText text_loginEmail;
     EditText text_loginPassword;
     ImageView imgView;
 
+    private List<AuthUI.IdpConfig> providers;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build());
+
+        Login();
 
         btn_login = findViewById(R.id.btn_login);
         text_loginEmail = findViewById(R.id.text_loginEmail);
@@ -32,12 +51,31 @@ public class Login extends AppCompatActivity {
                 Login();
             }
         });
-
     }
 
-    private Boolean Login(){
-        Intent intent = new Intent(this, MyBrews.class);
-        startActivity(intent);
-        return true;
+    private void Login(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setLogo(R.drawable.cheers)
+                        .build(), RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Intent intent = new Intent(this, MyBrews.class);
+                startActivity(intent);
+            } else {
+            }
+        }
     }
 }
