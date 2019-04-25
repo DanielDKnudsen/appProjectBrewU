adb5a4582186fc1515334d818e31f331a5a22810
Daniel Knudsen <dk@idealdev.dk>
2019-04-25 12:52:32 +0200

    Added activities

---
+++ app/src/main/java/com/danieldk/brewu/AllBrews.java
@@ -0,0 +1,13 @@
+package com.danieldk.brewu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AllBrews extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_brews);
+    }
+}
