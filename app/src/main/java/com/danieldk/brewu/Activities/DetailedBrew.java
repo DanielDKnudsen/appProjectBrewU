779dbba63934a936224f4b4b04d6ecd024b7106e
Daniel Knudsen <dk@idealdev.dk>
2019-05-02 15:48:09 +0200

    imports

--- app/src/main/java/com/danieldk/brewu/Activities/DetailedBrew.java
+++ app/src/main/java/com/danieldk/brewu/Activities/DetailedBrew.java
@@ -3,6 +3,8 @@ package com.danieldk.brewu.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.danieldk.brewu.R;
+
 public class DetailedBrew extends AppCompatActivity {
 
     @Override
