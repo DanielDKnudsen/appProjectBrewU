db6a9da2d1c0ef531cf13d9c6f6d651a64dba672
LineBD <linebdanielsen@gmail.com>
2019-05-09 15:49:58 +0200

    Changed burgermenu again

--- app/src/main/java/com/danieldk/brewuappassignment2/Activities/NavigationMenu.java
+++ app/src/main/java/com/danieldk/brewuappassignment2/Activities/NavigationMenu.java
@@ -1,9 +1,6 @@
 package com.danieldk.brewuappassignment2.Activities;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.TransactionTooLargeException;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -21,13 +18,6 @@ import android.view.Menu;
 import com.danieldk.brewuappassignment2.Fragments.AllBrews;
 import com.danieldk.brewuappassignment2.Fragments.MyBrews;
 import com.danieldk.brewuappassignment2.R;
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class NavigationMenu extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -67,6 +57,28 @@ public class NavigationMenu extends AppCompatActivity
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -74,22 +86,17 @@ public class NavigationMenu extends AppCompatActivity
         int id = item.getItemId();
         transaction = fragmentManager.beginTransaction();
 
-        if (id == R.id.nav_home) {
+        if (id == R.id.nav_myBrews) {
             Fragment myBrews = new MyBrews();
             transaction.replace(R.id.container, myBrews);
 
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_allBrews) {
             Fragment allBrews = new AllBrews();
             transaction.replace(R.id.container, allBrews);
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_logOut) {
 
-        } else if (id == R.id.nav_tools) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+            //TODO: kode hertil
         }
         transaction.commit();
 
