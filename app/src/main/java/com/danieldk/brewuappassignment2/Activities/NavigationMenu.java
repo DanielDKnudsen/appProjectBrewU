df4ead4353ba2ce37f0c8676c8216486428e6280
laerkebjernemose <laerkebjernemose@hotmail.com>
2019-05-09 15:39:57 +0200

    Deleted floating button and Settings menu.

--- app/src/main/java/com/danieldk/brewuappassignment2/Activities/NavigationMenu.java
+++ app/src/main/java/com/danieldk/brewuappassignment2/Activities/NavigationMenu.java
@@ -3,12 +3,10 @@ package com.danieldk.brewuappassignment2.Activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.TransactionTooLargeException;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.v4.view.GravityCompat;
@@ -49,14 +47,7 @@ public class NavigationMenu extends AppCompatActivity
         transaction.add(R.id.container, myBrews);
         transaction.commit();
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -74,28 +65,6 @@ public class NavigationMenu extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
