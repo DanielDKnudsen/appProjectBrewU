94790c1eb8fca70448ff0e068cd267ee8f8cb93e
Thomas Kristensen <au566737@uni.au.dk>
2019-05-09 15:38:40 +0200

    createdAdaptor

--- app/src/main/java/com/danieldk/brewuappassignment2/Activities/Login.java
+++ app/src/main/java/com/danieldk/brewuappassignment2/Activities/Login.java
@@ -24,6 +24,7 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
         //Intent intent = new Intent(this, NavigationMenu.class);
         //startActivity(intent);
 
