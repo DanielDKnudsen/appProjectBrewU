c699cb931f87d320359a7d08acbd0a2197cbaf46
Daniel Knudsen <dk@idealdev.dk>
2019-05-15 10:25:53 +0200

    Can now search for brewsin all brews

--- app/src/main/java/com/danieldk/brewuappassignment2/Adaptor/BeerAdaptor.java
+++ app/src/main/java/com/danieldk/brewuappassignment2/Adaptor/BeerAdaptor.java
@@ -5,33 +5,41 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.danieldk.brewuappassignment2.Models.Brew;
 import com.danieldk.brewuappassignment2.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class BeerAdaptor extends BaseAdapter {
+public class BeerAdaptor extends BaseAdapter implements Filterable {
 
-    private List<Brew> BrewList;
     private Context context;
     private Brew brew;
+    private List<Brew> OriginalBrews;
+    private List<Brew> FilteredBrews;
 
     public BeerAdaptor(Context context, List<Brew> brews)
     {
         this.context = context;
-        this.BrewList = brews;
+        this.OriginalBrews = brews;
+        this.FilteredBrews = brews;
     }
 
     @Override
     public int getCount() {
-        return BrewList.size();
+        return FilteredBrews.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return BrewList.get(position);
+        return FilteredBrews.get(position);
     }
 
     @Override
@@ -47,25 +55,75 @@ public class BeerAdaptor extends BaseAdapter {
             LayoutInflater brewInflator = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = brewInflator.inflate(R.layout.brew_list_item, null);
         }
-        brew = BrewList.get(position);
+        brew = FilteredBrews.get(position);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
         if(brew != null)
         {
-            TextView txtTitle = convertView.findViewById(R.id.txtBrewTitle);
+            TextView txtTitle = convertView.findViewById(R.id.txtBrewName);
             TextView txtType = convertView.findViewById(R.id.txtBrewType);
+            TextView txtUser = convertView.findViewById(R.id.txtUserName);
+            RatingBar rating = convertView.findViewById(R.id.rating);
 
             txtTitle.setText(brew.getTitle());
             txtType.setText(brew.getBeerType());
+            rating.setNumStars((int)brew.getAvgRating());
+
+            if(user != null)
+            {
+                txtUser.setText(user.getDisplayName());
+            } else {
+                txtUser.setText("Unknown"); // TODO: Externalize string
+            }
         }
 
         return convertView;
     }
 
     public List<Brew> getBrewList() {
-        return BrewList;
+        return FilteredBrews;
     }
 
     public void setBrewList(List<Brew> brewList) {
-        BrewList = brewList;
+        OriginalBrews = brewList;
+    }
+
+    @Override
+    public Filter getFilter() {
+        Filter filter = new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults filterResults = new FilterResults();
+                List<Brew> Filtered = new ArrayList<Brew>();
+
+                if (OriginalBrews == null) {
+                    OriginalBrews = FilteredBrews;
+                }
+
+                if (constraint == null || constraint.length() == 0){
+                    filterResults.count = OriginalBrews.size();
+                    filterResults.values = OriginalBrews;
+                } else {
+                    constraint = constraint.toString().toLowerCase();
+                    for (int i = 0; i < OriginalBrews.size(); i++) {
+                        String data = OriginalBrews.get(i).getBeerType();
+                        if (data.toLowerCase().startsWith(constraint.toString())) {
+                            Filtered.add(OriginalBrews.get(i));
+                        }
+                    }
+                    filterResults.count = Filtered.size();
+                    filterResults.values = Filtered;
+                }
+
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                FilteredBrews = (List<Brew>) results.values;
+                notifyDataSetChanged();
+            }
+        };
+        return filter;
     }
 }
--- app/src/main/java/com/danieldk/brewuappassignment2/Fragments/AllBrews.java
+++ app/src/main/java/com/danieldk/brewuappassignment2/Fragments/AllBrews.java
@@ -1,18 +1,36 @@
 package com.danieldk.brewuappassignment2.Fragments;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
+import com.danieldk.brewuappassignment2.Adaptor.BeerAdaptor;
+import com.danieldk.brewuappassignment2.Models.Brew;
 import com.danieldk.brewuappassignment2.R;
 import com.danieldk.brewuappassignment2.ViewModels.BrewViewModel;
 
+import java.util.List;
+
 public class AllBrews extends Fragment {
+
+    private BrewViewModel mViewModel;
+    private ListView listViewAllBeers;
+    private List<Brew> brewList;
+    private BeerAdaptor beerAdaptor;
+    private ProgressBar loader;
+    private Context context;
+    private EditText searchBrew;
 
     public static AllBrews newInstance() {
         return new AllBrews();
@@ -21,7 +39,48 @@ public class AllBrews extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        context = container.getContext();
         return inflater.inflate(R.layout.all_brews_fragment, container, false);
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // TODO: Use the ViewModel
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        mViewModel = ViewModelProviders.of(this).get(BrewViewModel.class);
+        listViewAllBeers = view.findViewById(R.id.listAllBrews);
+        loader = view.findViewById(R.id.loader);
+        searchBrew = view.findViewById(R.id.SearchBrewType);
+        loader.setVisibility(view.VISIBLE);
+        mViewModel.getBrews();
+        mViewModel.loadBrews().observe(this,brews ->{
+            beerAdaptor = new BeerAdaptor(context,brews);
+            listViewAllBeers.setAdapter(beerAdaptor);
+            loader.setVisibility(view.GONE);
+        });
+
+        searchBrew.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                beerAdaptor.getFilter().filter(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
 
 }
